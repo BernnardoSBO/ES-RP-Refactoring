64eaabc31a78cd6012cfcd506ea50ec65ed1b07b
Bernnardo Seraphim <bernnardosbo@gmail.com>
2022-06-25 03:05:16 -0300

    Commit 18

--- TextStatement.java
+++ TextStatement.java
@@ -2,18 +2,17 @@ import java.util.Enumeration;
 
 public class TextStatement extends Statement {
 
-  public String getHeader(Customer aCustomer) {
+    public String getHeader(Customer aCustomer) {
     return "Rental Record for " + aCustomer.getName() + "\n";
   }
 
-  public String getRentalFigures(Rental aRental) {
-    return "\t" + aRental.getMovie().getTitle() + "\t" + String.valueOf(aRental.getCharge()) + "\n";
-  }
+    public String getRentalFigures(Rental aRental) {
+        return "\t" + aRental.getMovie().getTitle() + "\t" + String.valueOf(aRental.getCharge()) + "\n";
+    }
 
-  public String getFooter(Customer aCustomer) {
-    String result = "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
-    result += "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + " frequent renter points";
-    return result;
+    public String getFooter(Customer aCustomer) {
+        String result = "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
   }
-
 }
