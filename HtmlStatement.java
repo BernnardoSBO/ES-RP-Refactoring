64bffaf4b4b672e374d99f582ed3c618c983b081
Bernnardo Seraphim <bernnardosbo@gmail.com>
2022-06-25 03:04:11 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -21,4 +21,19 @@ public class HtmlStatement extends Statement {
         return result;
    }
 
+   public String getHeader(Customer aCustomer) {
+    return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+  }
+
+  public String getRentalFigures(Rental aRental) {
+    return aRental.getMovie().getTitle() + ": " + String.valueOf(aRental.getCharge()) + "<BR>\n";
+  }
+
+  public String getFooter(Customer aCustomer) {
+    String result = "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+    result += "On this rental you earned <EM>" + String.valueOf(aCustomer.getTotalFrequentRenterPoints())
+        + "</EM> frequent renter points<P>";
+    return result;
+  }
+
 }
