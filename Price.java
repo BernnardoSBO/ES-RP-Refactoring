33991eebc1d5775d1741bdf0819e4639aa95d3ce
Bernnardo Seraphim <bernnardosbo@gmail.com>
2022-06-25 02:44:39 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -1,5 +1,15 @@
 public abstract class Price {
-   public abstract int getPriceCode();
+    public static final int  CHILDRENS = 2;
+    public static final int  REGULAR = 0;
+    public static final int  NEW_RELEASE = 1;
+    public abstract int getPriceCode();
 
-   public abstract double getCharge(int daysRented);
+    public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else 
+            return 1;
+    }
 }
